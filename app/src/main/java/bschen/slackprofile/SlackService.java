a7c08cd8702cb626bb2a4bc42510d4b36057d56c
Ben Chen <bschen@yahoo-inc.com>
2016-01-16 20:25:21 -0800

    Updating token & url for SlackService

--- app/src/main/java/bschen/slackprofile/SlackService.java
+++ app/src/main/java/bschen/slackprofile/SlackService.java
@@ -7,9 +7,9 @@ import retrofit2.http.Query;
 
 public interface SlackService {
 
-    String BASE_URL = "https://api.slack.com/methods/";
-    String TOKEN = "xoxp-5048173296-5048346304-5180362684-7b3865";
+    String BASE_URL = "https://slack.com/api/";
+    String TOKEN = "xoxp-5048173296-5048487710-18650790535-1cc8644082";
 
-    @GET("members.list")
+    @GET("users.list")
     Call<MembersResponse> listMembers(@Query("token") String token);
 }
